3350beb856dd4dab964085564dc241b76b9713d5
Denys Strashniuk <d.strashniuk@gmail.com>
2025-04-22 09:13:02 +0200

    fix Aufgabe Lotto_6aus49

--- Aufgaben/10_Lotto_6aus49/Lotto_6aus49/Program.cs
+++ Aufgaben/10_Lotto_6aus49/Lotto_6aus49/Program.cs
@@ -1,4 +1,21 @@
-﻿namespace Lotto_6aus49
+﻿/*
+Schreiben Sie ein Lottoprogramm (6 aus 49)
+
+Erstellen sie eine Klasse "Lotto":
+Dieses soll ein Array enthalten, das zufallsbasiert 6 Gewinnzahlen beinhaltet.
+(Nutzen Sie dazu ein random-Objekt).
+
+Nun sollte der Anwender 6 Zahlen aus 49 auswählen, die mit den Gewinnzahlen
+verglichen werden müssen und es ausgegeben wird, wieviele "Richtige" er hat.
+
+Zu beachtende Schritte:
+1. User-Eingaben der 6 getippten Zahlen => Speicherung in einem Array
+2. 6 zufällige Zahlen zwischen 1 und 49 "ziehen" => Speicherung in einem neuen Array
+3. Vergleich der getippten und gezogenen Zahlen => Speicherung der Anzahl der "Richtigen"
+4. Ausgabe der gezogenen Zahlen, der getippten Zahlen und der Anzahl der "Richtigen"
+*/
+
+namespace Lotto_6aus49
 {
     class Program
     {
