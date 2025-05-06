19dedf09a85838bb1dcbcb6c1e7eaeffa69fa520
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-06 08:34:59 +0200

    hinzufügen: aufgabe für 13_Lagersystem

--- Aufgaben/13_Lagersystem/Lagersystem/Program.cs
+++ Aufgaben/13_Lagersystem/Lagersystem/Program.cs
@@ -1,4 +1,11 @@
-﻿using static System.Console;
+﻿/* Programmiere in C# nur mit der nutzung von Arrays folgendes:
+In einem Lagersystem hat 100 Plätze. Über ein Menü können wir 
+Bauteile und schrauben über die Konsole in unser Lagersysten eintragen. 
+Wenn ein Lagerplatz bereits belegt ist soll darauf verwiesen werden das element woander zu lagern. 
+Wir können über die Lagerplatznummer uns auch Elemente anfordern die wir dann entfernen können wen wir das wollen. 
+und wir müssen die möglichkeit haben Das Lager auszugeben in der Konsole.
+*/
+using static System.Console;
 
 namespace Lagersystem
 {
